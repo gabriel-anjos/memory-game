ca6ada8bb40bc05b7d8cee676c65638ed83f55e7
anjos-gabriel <anjos.gabriel999@gmail.com>
2021-12-28 16:42:40 -0300

    making buttons with colors dinamics

---
+++ src/components/flat-button/index.js
@@ -0,0 +1,35 @@
+const flatButton = (function(){
+    const module = {}
+
+    module._id=0;
+
+    module._style=(active)=>{
+        const $head = document.querySelector("head");
+        const $style = document.createElement("style");
+        $style.textContent=`
+        .flat-button-${module._id}{
+            background-color: ${active ? "#f25a70": "#eae6da"};
+            color:${active ?"#fff" : "#fffcee"};
+            font-size: 24px;
+            font-weight: bold;
+            border:none;
+            width:50%;
+            height:176px;
+            text-transform: uppercase;
+        }
+       
+        `;
+        $head.insertAdjacentElement("beforeend",$style);
+    };
+
+    module.render=(content="", active = false)=>{
+        // module._id = module._id + 1
+        module._id++;
+        module._style(active);
+        return `<button class="flat-button-${module._id}">${content}</button>`        
+    }
+
+    return{
+        render:module.render
+    }
+})()
---
+++ src/pages/signup/index.js
@@ -0,0 +1,9 @@
+(function(){
+    const $root = document.querySelector("#root");
+
+    const $loginButton = flatButton.render("Log in");
+    const $signupButton = flatButton.render("Sign up",true);
+
+    $root.insertAdjacentHTML("beforeend",$loginButton);
+    $root.insertAdjacentHTML("beforeend",$signupButton);
+})();
