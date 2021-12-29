b4c849cef51b14334753c19dfdc374f4529ce43a
anjos-gabriel <anjos.gabriel999@gmail.com>
2021-12-29 13:35:43 -0300

    children align

--- src/components/objects/logo-wrapper/index.js
+++ src/components/objects/logo-wrapper/index.js
@@ -1,7 +1,20 @@
 const logoWrapper = (function(){
     const module ={}
     
+    module._style=()=>{
+        const $head = document.querySelector("head");
+        const $style = document.createElement("style")
+        $style.textContent=`
+            .avatar-wrapper{
+                text-align:center;
+            }
+        
+        
+        `;
+        $head.insertAdjacentElement("beforeend",$style);
+    }
     module.render=(...$children)=>{
+        module._style();
 
         return`<div class="avatar-wrapper">${$children.join("")} </div>`
     }
